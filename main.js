f1bcd0398a5c9f99a782ccf105e47788cca8071c
lostmart <lostmart@hotmail.com>
2024-10-27 22:03:43 +0100

    functions adding stuff

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 "use strict"
 import { buttonComp } from "./components/buttonComp.js"
 
+let result = 0
+
+const showResult = (result) => {
+	const divCont = document.createElement("div")
+	divCont.classList.add("result")
+	divCont.textContent = result
+	return divCont
+}
+
 app.appendChild(buttonComp("click", "btn-primary", clickOne))
 app.appendChild(buttonComp("test", "btn-secondary", clickTwo))
 app.appendChild(buttonComp("test deux", "btn-danger", clickThree))
 
+app.appendChild(showResult(result))
+
 function clickOne() {
-	console.log("click button one!")
+	result++
+	document.querySelector(".result").textContent = result
 }
 
 function clickTwo() {
-	console.log("click button two!")
+	result--
+	document.querySelector(".result").textContent = result
 }
 
 function clickThree() {
-	console.log("click button three!")
+	result = 0
+	document.querySelector(".result").textContent = result
 }
