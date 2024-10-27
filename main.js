3e1fa1e221f66ea67dddb7353ca74324906d0588
lostmart <lostmart@hotmail.com>
2024-10-27 20:07:21 +0100

    button comp try

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 "use strict"
+
+const buttonComp = document.createElement("button")
+buttonComp.innerText = "click"
+buttonComp.classList.add("btn")
+buttonComp.addEventListener("click", () => {
+	console.log("clicked the btn !!")
+})
+
+app.appendChild(buttonComp)
