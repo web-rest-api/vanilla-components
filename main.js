ab2ab6934dc067c38c7928cb4bcabe09aac2ecff
lostmart <lostmart@hotmail.com>
2024-10-28 19:13:52 +0100

    new styles and stff

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ import { clickOne, clickThree, clickTwo } from "./utils/functions.js"
 
 window.result = 0
 
-app.appendChild(buttonComp("Add", "btn-primary", clickOne))
 app.appendChild(buttonComp("Subtract", "btn-secondary", clickTwo))
+app.appendChild(buttonComp("Add", "btn-primary", clickOne))
 app.appendChild(buttonComp("Reset", "btn-danger", clickThree))
 
 app.appendChild(showResult(result))
